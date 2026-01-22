7391a7f90f6ea5bb94261fb55e513e73b3d74717
nimur40 <nimur40@gmail.com>
2026-01-22 19:00:55 +0600

    22 jan

--- Loop/loop.js
+++ Loop/loop.js
@@ -21,19 +21,62 @@
 // }
 
 
+// let person={
+//     name:"nimur",
+//     age:30, 
+//     marks:60,
+//     result:function(){
+//         return this.marks>50?"pass":"fail";
+//     } 
+// };
+// console.log(person.result());
+// let employee=[
+//     {name:"nimur",salary:1000},
+//     {name:"rakib",salary:2000},
+//     {name:"sada",salary:4000},
+//     {name:"tre",salary:5000}
+// ]
+// employee.forEach(items => {
+//     console.log(items.name+"="+items.salary)
+// })
+// let employees = [
+//   { name: "Nimur", salary: 25000 },
+//   { name: "Rana", salary: 35000 },
+//   { name: "Asz", salary: 45000 }
+// ];
+// let highSalary=employees.filter(hs => hs.salary >30000);
+// highSalary.forEach(e => console.log(e.name))
+
+// let user = {
+//   name: "Nimur",
+//   age: 25,
+//   city: "Dhaka"
+// };
+// document.getElementById("demo").innerHTML=
+// "Name: " + user.name +"<br>"+
+// "Age: " +user.age +"<br>"+
+// "City:" +user.city+"<br>";
+// ;
 let student = {
-  name: "Rahim",
-  math: 80,
-  english: 70,
-  science: 90
+    name: "Rana",
+    marks: 78
 };
 
-let total=0;
-for(let key in student){
-    if(typeof student[key]==="number"){
-        total=total+student[key];
-    }
-   
+function showInfo() {
+    document.getElementById("info").innerHTML =
+        student.name + " score " + student.marks;
 }
- console.log("total number is ="+total);
 
+let products = [
+  { name: "Pen", price: 10 },
+  { name: "Book", price: 120 },
+  { name: "Bag", price: 800 }
+];
+let ul=document.getElementById("list");
+products.forEach(item =>
+{
+    let li=document.createElement("li");
+    li.innerText=item.name+"-"+item.price;
+    ul.appendChild(li);
+}
+)
